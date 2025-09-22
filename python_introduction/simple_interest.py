066f4e50da6c9b99591e28d0e14ce4939ed6ea5e
Evans Omia <evansomia005@gmail.com>
2025-09-22 17:23:41 +0400

    added new files

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,20 @@
-principal = 1000
-rate = 0.05
-time = 3
-interest = principal* rate * time
-print(f"The simple interest is : {interest}")
+import random
+
+def play_game():
+  secret_number = random.randint(1, 10)
+  guess = int(input("Please guess the number to be generated: "))
+
+  match guess:
+     case guess if secret_number == guess:
+        print("Congratulations, You guessed correctly")
+     case guess if secret_number < guess:
+         print("Oops, your guess is a bit high. Try again!")
+     case guess if guess < secret_number:
+          print("Nope, your guess is a bit low. Give it another shot!")
+
+while True:
+    play_game()
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing! Goodbye 👋")
+        break
