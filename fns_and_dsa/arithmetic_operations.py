e72386bc9b7617cb5eabc55cd09a3ff228bba655
Evans Omia <evansomia005@gmail.com>
2025-10-04 22:27:13 +0400

    Update arithmetic_operations.py to add arithmetic functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,14 @@
-namespace 
+def perform_operation(num1: float, num2:float, operation:str):
+  if operation == "add":
+    return num1 + num2
+  elif operation == "subtract"
+    return num1 - num2
+  elif operation == "multiply":
+    return num1 * num2
+  elif operation == "divide":
+    if num1 == 0 or num2 == 0:
+      return "Error: Division by zero"
+    else:
+      return num1 / num2
+  else
+     return "Error: Invalid operation"
