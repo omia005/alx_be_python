03cb29c486ab7c8793d3907551b8e95ba62bcfd1
Evans Omia <evansomia005@gmail.com>
2025-10-11 09:18:06 +0400

    Create robust_division_calculator.py

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,12 @@
+def safe_divide(numerator, denominator):
+  try:
+        num = float(numerator)
+        den = float(denominator)
+    except ValueError:
+        return "Error: Both inputs must be numeric."
+    
+    try:
+        result = num / den
+        return str(result)
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
