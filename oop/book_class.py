13112b4981f025615dbb51d50a4cda938b5d2e52
Evans Omia <evansomia005@gmail.com>
2025-10-12 09:24:55 +0400

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,7 +4,7 @@ class Book:
      self.author = author
 
   def __del__(self):
-      return f"Deleting {title}"
+      return f"Deleting {self.title}"
 
   def __str__(self):
       return  f"{self.title} by {self.author} published in {self.year}"
