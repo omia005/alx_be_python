de88abefb912e9319e041223a2a91bd53cd42061
Evans Omia <evansomia005@gmail.com>
2025-10-12 09:26:00 +0400

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,6 +2,7 @@ class Book:
   def __init__(self,title,author,year):
      self.title = title
      self.author = author
+     self.year = year
 
   def __del__(self):
       return f"Deleting {self.title}"
